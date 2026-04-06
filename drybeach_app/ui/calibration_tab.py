a1301563faf4dbc49cf1532b7c6e0dca562d780d
Defly Wang <defly@yeah.net>
2026-04-06 23:40:12 +0800

    修复calibration_tab.py缺少pyqtSignal导入的错误

--- drybeach_app/ui/calibration_tab.py
+++ drybeach_app/ui/calibration_tab.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout, QPushButton, 
                              QLabel, QDoubleSpinBox)
+from PyQt6.QtCore import pyqtSignal
 
 
 class CalibrationTab(QWidget):
